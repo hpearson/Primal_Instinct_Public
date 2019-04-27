a96dd5bb013be3dd81f64a40404742c2c917de90
starshiptrooper0589 <starshiptrooper0589@gmail.com>
2019-04-27 03:04:35 -0600

    UserID's are now stored with Error reports to identify who files the report (So I can email follow up emails)

--- app/models/Errors.php
+++ app/models/Errors.php
@@ -9,11 +9,17 @@ class Errors_Model {
     }
 
     public function CreateErrorReport($data) {
+        // Allow NULL Players to file a report
+        $player = Session::get('PlayerGUID');
+        if (is_array($player)){
+            $player = null;
+        }
         $this->db->query(
                   "INSERT INTO UserReports "
-                . "(ReportText) VALUES (:report) "
+                . "(ReportText, Reporter) VALUES (:report, :user) "
                 );
         $this->db->bind('report', $data['report']);
+        $this->db->bind('user', $player);
         return $this->db->execute();
     }
 
