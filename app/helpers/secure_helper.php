b4e4412b7253469027c52b03a3595f52fff6f140
starshiptrooper0589 <starshiptrooper0589@gmail.com>
2019-04-27 09:07:05 -0600

    Updated security functions to accept both arrays and strings Applied updated security onto Username display

--- app/helpers/secure_helper.php
+++ app/helpers/secure_helper.php
@@ -3,8 +3,15 @@
 class Secure {
 
     static function HTML($input) {
-        foreach ($input as $key => $val) {
-            $input[$key] = htmlspecialchars($val, ENT_QUOTES);
+        // If data is an array secure all values
+        if (is_array($input)){
+            foreach ($input as $key => $val) {
+                $input[$key] = htmlspecialchars($val, ENT_QUOTES);
+            }            
+        }
+        // If data is a string secure its text
+        if (is_string($input)){
+            $input = htmlspecialchars($input, ENT_QUOTES);
         }
         return $input;
     }
--- app/views/game/_nearplayers.php
+++ app/views/game/_nearplayers.php
@@ -4,7 +4,7 @@ if ( count($data) > 0){
     echo 'There are '.count($data).' players nearby';
     echo '<br>';
     foreach ($data as $value) {
-        echo $value->Username.'; ';
+        echo Secure::HTML($value->Username).'; ';
     }
 } else {
     echo 'Nobody is nearby';
